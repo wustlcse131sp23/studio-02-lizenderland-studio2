cb0637027c4fa7f8b27821598b9a947b9002b376
miaom <miaom@LAPTOP-NVIE6NS0.dhcp.wustl.edu>
2023-02-02 13:25:02 -0600

    Ruin

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,43 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class Ruin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		double startAmount, winChance, winLimit;
+		System.out.println("Starting amount of money in $?");
+		startAmount = in.nextDouble();
+		System.out.println("Chance of winning in %?");
+		winChance = in.nextDouble();
+		System.out.println("Limit of winning in $?");
+		winLimit = in.nextDouble();
+
+		double money = startAmount;
+		while (money < winLimit && money > 0) {
+
+			double chance = Math.random() * 100;
+			if (chance <= winChance) {
+				money++;
+				System.out.println("Today is a win. Balance is $" + money);
+			} else {
+				money--;
+				System.out.println("Today is a loss. Balance is $" + money);
+			}
+		}
+		if (money == winLimit) {
+			System.out.println("Win limit reached, current balance is $" + money);
+		} else if (money == 0) {
+			System.out.println("Lost all money");
+		} else {
+			System.out.println("SOMETHING IS WRONG!!!");
+		}
+
+
+
+	}
+}
+
+
