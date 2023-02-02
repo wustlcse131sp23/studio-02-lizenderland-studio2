29c1e62dbbbc8a3cd0d1c78110afd191011ed16d
miaom <miaom@LAPTOP-NVIE6NS0.dhcp.wustl.edu>
2023-02-02 14:18:48 -0600

    Ruin final

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -17,6 +17,7 @@ public class Ruin {
 		System.out.println("How many days will you play?");
 		int totalSimulations = in.nextInt();
 		int day = 1;
+		int loss = 0;
 		while (day <= totalSimulations) {
 			int play = 0;
 			double money = startAmount;
@@ -36,6 +37,7 @@ public class Ruin {
 				System.out.println("Win limit reached, current balance is $" + money);
 			} else if (money == 0) {
 				System.out.println("Lost all money");
+				loss++;
 			} else {
 				System.out.println("SOMETHING IS WRONG!!!");
 			}
@@ -44,6 +46,19 @@ public class Ruin {
 			System.out.println("The number of plays today was:" + play);
 		}
 
+		double simRuinRate = ((double) loss/(day - 1)) * 100;
+		System.out.println("Simulated ruin rate is " + simRuinRate + "%");
+
+
+		double a = (1 - (winChance/100)) / (winChance/100);
+		double expRuin;
+		if (winChance == 50) {
+			expRuin = 1-(startAmount / winLimit);
+		} else {
+			expRuin = (Math.pow(a, startAmount) - Math.pow(a, winLimit)) / (1-Math.pow(a, winLimit));
+		}
+		expRuin = expRuin * 100;
+		System.out.println("Expected ruin rate is " + expRuin + "%");
 
 	}
 }
